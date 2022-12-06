63c6261d9c335cc7f9d8b3b7e0c73d893b1755ce
Jiyeon Lee <ljy9969@gmail.com>
2022-12-07 02:15:45 +0900

    add react_frontend

--- 04_API/NewsDjango/main/urls.py
+++
@@ -1,9 +0,0 @@
-# Main URLConf(http://127.0.0.1:8000/main)
-from django.urls import path, include
-from . import views
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
